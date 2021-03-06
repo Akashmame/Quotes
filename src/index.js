bdec0683a7be51f0b5e79f9130d4ccc007ad55ef
Akashmame <79107396+Akashmame@users.noreply.github.com>
2021-03-06 20:01:02 +0530

    created successfully

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+// import App from './Projects/Amazon/App'
+// import App from './Tmp'
+// import 
+// import App from './img'
+
+import App from './Quotes/App'
+
+ReactDom.render(
+    <BrowserRouter>
+      <App /> 
+     </BrowserRouter>
+     ,document.getElementById('root')
+)
+
+
+
+
+
+
+
+
+
